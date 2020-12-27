d4802d8de3e6fe50727ff7c7696a712496162dae
foong <foong@adobe.com>
2020-12-27 03:03:27 -0700

    add rules

--- src/App.tsx
+++ src/App.tsx
@@ -138,6 +138,179 @@ const PlayerForm = ({ player, dispatch }) => {
   );
 };
 
+const Definition = ({ node }) => {
+  return (
+    <div className="definition-container">
+      <DefinitionItem node={node} />
+    </div>
+  );
+};
+
+const DefinitionItem = ({ node }) => {
+  let { label, subLabel, description, children } = node;
+  return (
+    <div className="definition">
+      <div className="definition-block">
+        <code className="definition-label">{label}</code>
+        {subLabel && <span> - {subLabel}</span>}
+        <div className="definition-desc">{description}</div>
+        {children?.map((child) => (
+          <DefinitionItem node={child} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+let tree = {
+  label: 'state',
+  subLabel: <code>{`{cells, history}`}</code>,
+  description: 'Game state object',
+  children: [
+    {
+      label: 'cells',
+      subLabel: <code>{`{id, symbol}[]`}</code>,
+      description: 'Board state represented by an array of cells',
+      children: [
+        {
+          label: 'id',
+          subLabel: <code>number</code>,
+          description:
+            'Cell number, as indicated at the bottom of each cell on the game board',
+        },
+        {
+          label: 'symbol',
+          subLabel: <code>string</code>,
+          description: 'Player symbol currently occupying the cell',
+        },
+      ],
+    },
+    {
+      label: 'history',
+      subLabel: <code>{`{player, cell, status}[]`}</code>,
+      description: 'List of moves taken during the game',
+      children: [
+        {
+          label: 'player',
+          subLabel: <code>{`{id, symbol, name}[]`}</code>,
+          description: 'Player information',
+
+          children: [
+            {
+              label: 'id',
+              subLabel: <code>string</code>,
+              description: 'Player ID',
+            },
+            {
+              label: 'symbol',
+              subLabel: <code>string</code>,
+              description: 'Symbol used to identify player and mark cells',
+            },
+            {
+              label: 'name',
+              subLabel: <code>string</code>,
+              description: 'Player name',
+            },
+          ],
+        },
+        {
+          label: 'cell',
+          subLabel: <code>number</code>,
+          description: 'Cell ID the player attempted to play on',
+        },
+        {
+          label: 'status',
+          subLabel: <code>string("SUCCESS"|"FAILURE")</code>,
+          description: `Status of player's move`,
+        },
+      ],
+    },
+  ],
+};
+
+let answer = {
+  label: 'answer',
+  subLabel: <code>{`{position}`}</code>,
+  description: 'Expected response object',
+  children: [
+    {
+      label: 'position',
+      subLabel: <code>number</code>,
+      description: 'Cell ID to play on',
+    },
+  ],
+};
+
+const Rules = () => {
+  return (
+    <div className="rules">
+      <label id="rules-link-label" htmlFor="rules-link">
+        how to play
+      </label>
+      <div id="rules">
+        <h3>Introduction</h3>
+        <p>
+          Tic Tac Toe FIGHT is a game where you can pit your own bot against
+          another in a Tic Tac Toe showdown!
+        </p>
+        <p>Begin by selecting the type of player from the dropdown list.</p>
+        <ul>
+          <li>Human: Play manually as a human player.</li>
+        </ul>
+        <p>
+          All other options are bot (automated) players. The bot is essentially
+          a function that takes in the game state and returns the position it
+          wants the game to play on. This will be further explained below.
+        </p>
+        <ul>
+          <li>
+            Gist: A link to the bot. An easy way to host the code for your bot
+            is by using GitHub. Simply provide the link to the raw version of
+            the file or gist, like{' '}
+            <a
+              target="_blank"
+              href="https://gist.githubusercontent.com/adrfoong/dad4a17fde19899a55c9e0cadfd86fef/raw/9522bbdc5f33885fbcd1719ab6d704727073cf6f/test.js"
+            >
+              this
+            </a>
+            .
+          </li>
+          <li>
+            Custom: Define the function directly in the textbox provided. Not
+            quite as user friendly as an editor, but functional.
+          </li>
+          <li>
+            Preset: A pre-defined bot. Chooses the first available cell to play
+            on.
+          </li>
+        </ul>
+        <h3>Defining a bot</h3>
+
+        <p>Your bot should look something like this:</p>
+        <code>
+          {`let bot = (state) => {
+            return { position: 1}
+          }`}
+        </code>
+        <p>
+          It should accept a <code>state</code> object which holds the
+          information it'll need to decide on its move:
+        </p>
+        <p>
+          <Definition node={tree} />
+        </p>
+        <p>
+          Remember to return an object, we'll call this object an{' '}
+          <code>answer</code>, which will tell the game which cell to play on:
+        </p>
+        <p>
+          <Definition node={answer} />
+        </p>
+      </div>
+    </div>
+  );
+};
+
 function App({}: AppProps) {
   let [state, dispatch] = useReducer(reducer, players);
 
@@ -145,10 +318,14 @@ function App({}: AppProps) {
     <div className="app">
       <h1>Tic Tac Toe FIGHT!</h1>
       <Game game={new GameManager(state)} />
+
       <div className="form">
         <PlayerForm player={state[0]} dispatch={dispatch} />
         <PlayerForm player={state[1]} dispatch={dispatch} />
       </div>
+      <input type="checkbox" id="rules-link" />
+
+      <Rules />
     </div>
   );
 }
